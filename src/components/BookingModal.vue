701ff32f62306d6489a06b326c2958b3f3d668ef
Muhammad Abdul Muhaymien Salaam <muhaymien96@gmail.com>
2022-05-25 18:08:50 +0200

    updates

--- src/components/BookingModal.vue
+++ src/components/BookingModal.vue
@@ -1,28 +1,105 @@
 <template>
   <div class="container">
-    <h1>Availability for {{ " " + city }}</h1>
-    <div class="row">
-      <div class="col-4" v-for="(dates, index) in dates" :key="index">
-        <div class="date-card">
-          <span class="dates ms-4 pt-3"
-            ><strong>{{ dates }}</strong></span
-          >
-          <Button
-            @click="toggleDate(dates)"
-            icon="pi pi-calendar-plus"
-            label="Book Now"
-            class="p-button-rounded ms-4 mt-2"
-            style="width: 12rem"
-          />
+    <div v-if="city === 'Cape Town'">
+      <h1>Availability for {{ " " + city }}</h1>
+      <div class="row">
+        <div class="col-4" v-for="cape in cptAvail" :key="cape">
+          <div class="date-card">
+            <span class="dates ms-4 pt-3"
+              ><strong>{{ cape.date }}</strong></span
+            ><br />
+            <span v-if="cape.remaining > 0" class="status ms-4 pt-3"
+              ><strong>{{ cape.remaining + " seats left" }}</strong></span
+            >
+            <span v-else class="status ms-4 pt-3"
+              ><strong>Sold Out</strong></span
+            >
+            <Button
+              @click="toggleDate(cape.date)"
+              icon="pi pi-calendar-plus"
+              label="Book Now"
+              class="p-button-rounded ms-4 mt-2"
+              style="width: 12rem"
+              :disabled="cape.remaining === 0"
+            />
+          </div>
         </div>
       </div>
+      <Button
+        @click="toggleCardsActive"
+        label="Go Back"
+        class="p-button-rounded ms-4 mt-2"
+        style="width: 12rem"
+      />
     </div>
-    <Button
-      @click="toggleCardsActive"
-      label="Go Back"
-      class="p-button-rounded ms-4 mt-2"
-      style="width: 12rem"
-    />
+    <!-- Johannesburg -->
+
+    <div v-if="city === 'Johannesburg'">
+      <h1>Availability for {{ " " + city }}</h1>
+      <div class="row">
+        <div class="col-4" v-for="joburg in jhbAvail" :key="joburg">
+          <div class="date-card">
+            <span class="dates ms-4 pt-3"
+              ><strong>{{ joburg.date }}</strong></span
+            ><br />
+            <span v-if="joburg.remaining > 0" class="status ms-4 pt-3"
+              ><strong>{{ joburg.remaining + " seats left" }}</strong></span
+            >
+            <span v-else class="status ms-4 pt-3"
+              ><strong>Sold Out</strong></span
+            >
+            <Button
+              @click="toggleDate(joburg.date)"
+              icon="pi pi-calendar-plus"
+              label="Book Now"
+              class="p-button-rounded ms-4 mt-2"
+              style="width: 12rem"
+              :disabled="joburg.remaining === 0"
+            />
+          </div>
+        </div>
+      </div>
+      <Button
+        @click="toggleCardsActive"
+        label="Go Back"
+        class="p-button-rounded ms-4 mt-2"
+        style="width: 12rem"
+      />
+    </div>
+    <!-- Durban  -->
+
+    <div v-if="city === 'Durban'">
+      <h1>Availability for {{ " " + city }}</h1>
+      <div class="row">
+        <div class="col-4" v-for="durbs in dbnAvail" :key="durbs">
+          <div class="date-card">
+            <span class="dates ms-4 pt-3"
+              ><strong>{{ durbs.date }}</strong></span
+            ><br />
+            <span v-if="durbs.remaining > 0" class="status ms-4 pt-3"
+              ><strong>{{ durbs.remaining + " seats left" }}</strong></span
+            >
+            <span v-else class="status ms-4 pt-3"
+              ><strong>Sold Out</strong></span
+            >
+            <Button
+              @click="toggleDate(durbs.date)"
+              icon="pi pi-calendar-plus"
+              label="Book Now"
+              class="p-button-rounded ms-4 mt-2"
+              style="width: 12rem"
+              :disabled="durbs.remaining === 0"
+            />
+          </div>
+        </div>
+      </div>
+      <Button
+        @click="toggleCardsActive"
+        label="Go Back"
+        class="p-button-rounded ms-4 mt-2"
+        style="width: 12rem"
+      />
+    </div>
   </div>
 
   <SeatsModal v-if="isModalOpen" />
@@ -36,7 +113,11 @@ import SeatsModal from "./SeatsModal.vue";
 export default {
   name: "BookingModal",
   data() {
-    return {};
+    return {
+      remainCPT: [],
+      remainJHB: [],
+      remainDBN: [],
+    };
   },
   components: {
     Button,
@@ -48,11 +129,15 @@ export default {
       city: (state) => state.booking.city,
       venue: (state) => state.booking.venue,
       isModalOpen: (state) => state.booking.isModalOpen,
+      cptAvail: (state) => state.booking.cptAvail,
+      jhbAvail: (state) => state.booking.jhbAvail,
+      dbnAvail: (state) => state.booking.dbnAvail,
     }),
   },
   methods: {
     ...mapMutations(["toggleModal", "toggleCardsActive", "toggleDate"]),
   },
+  beforeMount() {},
 };
 </script>
 
@@ -64,5 +149,8 @@ export default {
   background-color: aliceblue;
   margin-bottom: 10%;
   border-radius: 5%;
+}
+
+@media screen and (max-width: 430px) {
 }
 </style>
